8d18918f69fb3462b8fc69adbc06b68eca4f2ac5
OverNight <hidden98716@gmail.com>
2025-10-12 13:57:54 +0900

    initial settings

---
+++ src/main/java/moe/overnight/my_sebastian/MySebastianApplication.java
@@ -0,0 +1,13 @@
+package moe.overnight.my_sebastian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySebastianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySebastianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moe/overnight/my_sebastian/MySebastianApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.overnight.my_sebastian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySebastianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
