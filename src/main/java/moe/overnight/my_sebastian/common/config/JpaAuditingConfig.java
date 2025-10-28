b94ce94291324736777eca69525c5ec5500323a0
OverNight <hidden98716@gmail.com>
2025-10-28 20:01:42 +0900

    feat: JpaAuditingConfig 추가
    (QJXW-15)

---
+++ src/main/java/moe/overnight/my_sebastian/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package moe.overnight.my_sebastian.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
